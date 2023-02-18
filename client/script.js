1a0f3b6b9c0e64e1d9562a133828680aa284dc3b
RhannaCoopman <rhancoop@student.arteveldehs.be>
2023-02-18 18:20:09 +0100

    server update

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
   const response = await fetch('https://rhanna-chatgpt.onrender.com', {
-    method: "POST",
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
