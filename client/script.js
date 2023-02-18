e625bd4034582d0afaafd09eaad9a72f2791444c
RhannaCoopman <rhancoop@student.arteveldehs.be>
2023-02-18 18:44:06 +0100

    api key deleted

--- client/script.js
+++ client/script.js
@@ -81,8 +81,8 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch('https://rhanna-chatgpt.onrender.com', {
-    method: 'POST',
+  const response = await fetch('http://localhost:5000', {
+    method: "POST",
     headers: {
       'Content-Type': 'application/json',
     },
